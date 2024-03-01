941093711b068e8b238da223ddb5b18dff5faffd
dileep92s <dileep92s@gmail.com>
2024-03-01 11:12:17 +0530

    read prev data from db

--- webServ.py
+++ webServ.py
@@ -1,37 +1,48 @@
-from flask import Flask, render_template, request, jsonify
-from datetime import datetime, timedelta
+import subprocess
+
+required_packages = ["Flask", "Flask-SQLAlchemy", "pandas", "openpyxl"]
+missing_packages = [
+    package
+    for package in required_packages
+    if not subprocess.run(["pip", "show", package], stdout=subprocess.PIPE).returncode
+    == 0
+]
+if missing_packages:
+    subprocess.run(["pip", "install"] + missing_packages)
+
+# ------------------------------------------------------------------------------- #
+
+from flask import Flask, render_template, request, send_file, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
+import pandas as pd
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flowreport.db"
 db = SQLAlchemy(app)
 
 
+getToday = lambda: datetime.today()
+getYesterday = lambda: getToday() - timedelta(days=1)
+dateToday = lambda: getToday().strftime("%d/%m/%Y")
+dateYesterday = lambda: getYesterday().strftime("%d/%m/%Y")
+
+
 class ReportOne(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    kchPrev = db.Column(db.Float)
+    dateCurr = db.Column(db.String(10), primary_key=True)
     kchCurr = db.Column(db.Float)
-    ecInPrev = db.Column(db.Float)
     ecInCurr = db.Column(db.Float)
-    ecOutPrev = db.Column(db.Float)
     ecOutCurr = db.Column(db.Float)
-    tankPrev = db.Column(db.Float)
     tankCurr = db.Column(db.Float)
 
 
 class ReportTwo(db.Model):
-    date = db.Column(db.Integer, primary_key=True)
-    mainPrev = db.Column(db.Float)
+    dateCurr = db.Column(db.String(10), primary_key=True)
     mainCurr = db.Column(db.Float)
-    kchPrev = db.Column(db.Float)
     kchCurr = db.Column(db.Float)
-    ecPrev = db.Column(db.Float)
     ecCurr = db.Column(db.Float)
-    tankPrev = db.Column(db.Float)
     tankCurr = db.Column(db.Float)
-    dlfPrev = db.Column(db.Float)
     dlfCurr = db.Column(db.Float)
-    vblPrev = db.Column(db.Float)
     vblCurr = db.Column(db.Float)
 
 
@@ -40,14 +51,53 @@ def index():
     return render_template("index.html")
 
 
+def export_excel(table, reportName):
+    filename = dateToday().replace("/", "") + "_" + reportName + ".xlsx"
+    engine = db.engine
+    query = db.session.query(table)
+    df = pd.read_sql(query.statement, engine)
+    df.to_excel(filename, index=False)
+    return send_file(filename, as_attachment=True)
+
+
+@app.route("/export_kch")
+def export_kch():
+    return export_excel(ReportOne, "report_kch")
+
+
+@app.route("/export_two")
+def export_two():
+    return export_excel(ReportTwo, "report_two")
+
+
 @app.route("/report_kch")
 def report_kch():
-    reportKey = int(datetime.today().strftime("%d%m%Y"))
-    row = db.session.execute(db.select(ReportOne).filter_by(date=reportKey)).scalar_one()
-    print(row)
-    if row:
-        print(row.date, row.kchCurr, row.ecInCurr, row.ecOutCurr, row.tankCurr)
-    return render_template("report_kch.html")
+    dataPrev = (0, 0, 0, 0, 0, 0, 0, 0)
+    dataYesterday = ReportOne.query.filter_by(dateCurr=dateYesterday()).first()
+    if dataYesterday:
+        dataToday = ReportOne.query.filter_by(dateCurr=dateToday()).first()
+        if dataToday:
+            dataPrev = (
+                dataYesterday.kchCurr,
+                dataToday.kchCurr,
+                dataYesterday.ecInCurr,
+                dataToday.ecInCurr,
+                dataYesterday.ecOutCurr,
+                dataToday.ecOutCurr,
+                dataYesterday.tankCurr,
+                dataToday.tankCurr,
+            )
+        else:
+            dataPrev = (
+                dataYesterday.kchCurr,
+                0,
+                dataYesterday.ecInCurr,
+                0,
+                dataYesterday.ecOutCurr,
+                0,
+                dataYesterday.tankCurr,
+            )
+    return render_template("report_kch.html", dataPrev=dataPrev)
 
 
 @app.route("/submit_kch", methods=["POST"])
@@ -62,11 +112,8 @@ def submit_kch():
     tankPrev = float(request.form["tankPrev"]) if request.form["tankPrev"] else 0
     tankCurr = float(request.form["tankCurr"]) if request.form["tankCurr"] else 0
 
-    dateToday = datetime.today()
-    formattedToday = dateToday.strftime("%d/%m/%Y")
-
-    dateYesterday = dateToday - timedelta(days=1)
-    formattedYesterday = dateYesterday.strftime("%d/%m/%Y")
+    formattedToday = dateToday()
+    formattedYesterday = dateYesterday()
 
     totalMLD = (kchCurr - kchPrev) / 1000
     kchOutflow = f"KCH outflow to EC <br>{formattedYesterday} : {kchPrev}<br>{formattedToday} : {kchCurr}<br>Total MLD    : {totalMLD:.3f}"
@@ -84,19 +131,24 @@ def submit_kch():
 
     report = f"Electronic City<br><br>{kchOutflow}<br><br>{ecInflow}<br><br>{ecOutflow}<br><br>{tankLevel}"
 
-    reportKey = int(dateToday.strftime("%d%m%Y"))
-    reportOne = ReportOne(
-        date=reportKey,
-        kchPrev=kchPrev,
+    reportOneYesterday = ReportOne(
+        dateCurr=formattedYesterday,
+        kchCurr=kchPrev,
+        ecInCurr=ecInPrev,
+        ecOutCurr=ecOutPrev,
+        tankCurr=tankPrev,
+    )
+    db.session.merge(reportOneYesterday)
+    db.session.commit()
+
+    reportOneToday = ReportOne(
+        dateCurr=formattedToday,
         kchCurr=kchCurr,
-        ecInPrev=ecInPrev,
         ecInCurr=ecInCurr,
-        ecOutPrev=ecOutPrev,
         ecOutCurr=ecOutCurr,
-        tankPrev=tankPrev,
         tankCurr=tankCurr,
     )
-    db.session.merge(reportOne)
+    db.session.merge(reportOneToday)
     db.session.commit()
 
     return report
@@ -104,7 +156,41 @@ def submit_kch():
 
 @app.route("/report_two")
 def report_two():
-    return render_template("report_two.html")
+    dataPrev = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    dataYesterday = ReportTwo.query.filter_by(dateCurr=dateYesterday()).first()
+    if dataYesterday:
+        dataToday = ReportTwo.query.filter_by(dateCurr=dateToday()).first()
+        dataPrev = (
+            dataYesterday.mainCurr,
+            dataToday.mainCurr,
+            dataYesterday.kchCurr,
+            dataToday.kchCurr,
+            dataYesterday.ecCurr,
+            dataToday.ecCurr,
+            dataYesterday.tankCurr,
+            dataToday.tankCurr,
+            dataYesterday.dlfCurr,
+            dataToday.dlfCurr,
+            dataYesterday.vblCurr,
+            dataToday.vblCurr,
+        )
+    else:
+        dataPrev = (
+            dataYesterday.mainCurr,
+            0,
+            dataYesterday.kchCurr,
+            0,
+            dataYesterday.ecCurr,
+            0,
+            dataYesterday.tankCurr,
+            0,
+            dataYesterday.dlfCurr,
+            0,
+            dataYesterday.vblCurr,
+            0,
+        )
+
+    return render_template("report_two.html", dataPrev=dataPrev)
 
 
 @app.route("/submit_two", methods=["POST"])
@@ -123,11 +209,8 @@ def submit_two():
     vblPrev = float(request.form["vblPrev"]) if request.form["vblPrev"] else 0
     vblCurr = float(request.form["vblCurr"]) if request.form["vblCurr"] else 0
 
-    dateToday = datetime.today()
-    formattedToday = dateToday.strftime("%d/%m/%Y")
-
-    dateYesterday = dateToday - timedelta(days=1)
-    formattedYesterday = dateYesterday.strftime("%d/%m/%Y")
+    formattedToday = dateToday()
+    formattedYesterday = dateYesterday()
 
     mainTotalMLD = (mainCurr - mainPrev) / 1000
     mainFlow = f"Main Flow at KCH GLR <br>{formattedYesterday} : {mainPrev}<br>{formattedToday} : {mainCurr}<br>Total MLD    : {mainTotalMLD:.3f}"
@@ -159,23 +242,28 @@ def submit_two():
 
     report = f"{mainFlow}<br><br>{inletKCH}<br><br>{ecReading}<br><br>{vblReading}<br><br>{tankLevel}<br><br>{kchflow}<br><br>{dlfReading}"
 
-    reportKey = int(dateToday.strftime("%d%m%Y"))
-    reportTwo = ReportTwo(
-        date=reportKey,
-        mainPrev=mainPrev,
+    reportTwoYesterday = ReportTwo(
+        dateCurr=formattedYesterday,
+        mainCurr=mainPrev,
+        kchCurr=kchPrev,
+        ecCurr=ecPrev,
+        tankCurr=tankPrev,
+        dlfCurr=dlfPrev,
+        vblCurr=vblPrev,
+    )
+    db.session.merge(reportTwoYesterday)
+    db.session.commit()
+
+    reportTwoToday = ReportTwo(
+        dateCurr=formattedToday,
         mainCurr=mainCurr,
-        kchPrev=kchPrev,
         kchCurr=kchCurr,
-        ecPrev=ecPrev,
         ecCurr=ecCurr,
-        tankPrev=tankPrev,
         tankCurr=tankCurr,
-        dlfPrev=dlfPrev,
         dlfCurr=dlfCurr,
-        vblPrev=vblPrev,
         vblCurr=vblCurr,
     )
-    db.session.merge(reportTwo)
+    db.session.merge(reportTwoToday)
     db.session.commit()
 
     return report
